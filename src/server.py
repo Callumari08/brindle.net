b3f3ec28cc37b9c0025552ca7bcc32f10ea0abba
cleggy2028 <157853537+cleggy2028@users.noreply.github.com>
2024-02-02 16:53:34 +0000

    Fix dumb silly dumb
    Fixes dumb moment for levitcus cornwall

--- src/server.py
+++ src/server.py
@@ -1,4 +1,6 @@
 import socket
+import threading
+
 
 name = input("Server Name: ")
 try:
@@ -20,18 +22,34 @@ s.listen(5)
 
 print("Socket is listening")
 
-while True:
-    # this varible is used so we can see what the client sent us
-    recieved = ""
-    # Establish connection with client.
-    c, addr = s.accept()
-    print('Got connection from', addr)
-    c.send(b"Thank you for using BrindleNet 1.0!")
-    c.send(b"You are now connected to " + name.encode())
-    # Continually print what the client sends until they give the exit code ('&exit')
-    while recieved != "&exit":
-        print(recieved)
-        recieved = c.recv(1024).decode()
-        # Send a command to the client, so that once multi-user contact is established, it will not freeze waiting for a response
-        c.send(b'&brindlesent')
+
+def serve():
+    while True:
+        # this varible is used so we can see what the client sent us
+        recieved = ""
+        # Establish connection with client.
+        c, addr = s.accept()
+        print('Got connection from', addr)
+        welcome = "Thank you for using BrindleNet 1.1!\nYou are now connected to " + name
+        c.send(welcome.encode())
+        # Continually print what the client sends until they give the exit code ('&exit')
+        while recieved != "&exit":
+            print(recieved)
+            recieved = c.recv(1024).decode()
+            # Send a command to the client, so that once multi-user contact is established, it will not freeze waiting for a response
+            c.send(b'&brindlesent')
     c.send(b'&exit')
+
+threadymcthreadface = threading.Thread(target = serve)
+threadymcthreadface2 = threading.Thread(target = serve)
+threadymcthreadface3 = threading.Thread(target = serve)
+threadymcthreadface4 = threading.Thread(target = serve)
+
+threadymcthreadface.start()
+threadymcthreadface2.start()
+threadymcthreadface3.start()
+threadymcthreadface4.start()
+threadymcthreadface.join()
+threadymcthreadface2.join()
+threadymcthreadface3.join()
+threadymcthreadface4.join()
